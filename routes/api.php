6c3d7c548be02466e58a85d487cb579f042bc407
HasalaKS <hskaushalya1@gmail.com>
2025-02-21 11:45:47 +0530

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::group(['prefix' => 'api'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+});
+
